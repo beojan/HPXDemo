1960b5f8b3167e09472da1de5e50dd3ac4b6461d
Beojan Stanislaus <bstanislaus@lbl.gov>
2022-03-29 15:55:41 -0700

    Add event support to HPX example

--- src/HPXSched.h
+++ src/HPXSched.h
@@ -22,60 +22,80 @@ using namespace hana::literals;
 #include <hpx/pack_traversal/unwrap.hpp>
 
 namespace sch {
+class input_tag {};
+template <class HS> struct Input {
+    using hana_tag = sch::input_tag;
+    HS name;
+
+    constexpr Input(HS name) : name(name) {}
+};
 template <class Key, class Inputs, class Func> auto Define(Key key, Inputs inputs, Func func) {
     static_assert(hana::is_a<hana::string_tag>(key), "Define's key must be a hana::string");
     static_assert(hana::Sequence<Inputs>::value, "Define's inputs must be a tuple");
     using func_ret_t = ct::return_type_t<Func>;
-    // Tuple items are: key, inputs, function to calculate, is_required, result
-    return hana::make_pair(key,
-                           hana::make_tuple(key, inputs, std::reference_wrapper<Func>{func}, false,
-                                            (hpx::shared_future<func_ret_t>*)(nullptr)));
+    using fut_p = hpx::shared_future<func_ret_t>*;
+    // Tuple items are: key, inputs tuple, function to calculate, required, func returning ref-to-ptr-to-future, prototype ptr-to-future
+    return hana::make_pair(key, hana::make_tuple(
+                               key, inputs, std::function{func}, false,
+                                      [](auto& ec) -> fut_p& { return ec.slot[Key{}]; }, fut_p{}));
 }
 
 template <class... Defs> class Sched {
   private:
     hana::map<Defs...> definitions;
+    using Keys = decltype(hana::keys(definitions));
+    using FutTypes = decltype(hana::transform(hana::values(definitions),
+                                              hana::reverse_partial(hana::at, hana::size_c<5>)));
 
   public:
+    struct ECBase {
+        decltype(hana::to_map(hana::zip_with(hana::make_pair, Keys{}, FutTypes{})))
+              slot = hana::to_map(hana::zip_with(hana::make_pair, Keys{}, FutTypes{}));
+    };
+
     Sched(Defs... defs) : definitions(hana::make_map(defs...)) {}
-    template <typename Key> auto& retrieve(Key key) {
-        hana::at_c<3>(definitions[key]) = true; // Record that we need to calculate this value
-        return hana::at_c<4>(definitions[key]); // Return reference to pointer to future
+    template <typename Key> auto& retrieve(ECBase& ec, Key key) {
+        static_assert(!hana::is_a<sch::input_tag>(key), "Cannot 'retrieve' an input");
+        hana::at_c<3>(definitions[key]) = true;     // Record that we need to calculate this value
+        return hana::at_c<4>(definitions[key])(ec); // Return reference to pointer to future
     }
 
     // This function does the scheduling (and running)
     // For now, lets use HPX
-    bool schedule() {
+    template <typename EC> bool schedule(EC& ec) {
         constexpr auto dataflow = BOOST_HOF_LIFT(hpx::dataflow);
         constexpr auto is_required = hana::reverse_partial(hana::at, hana::size_c<3>);
         // Given a key, schedule the computation for that value
-        auto run = hana::fix([this, &dataflow](auto self, auto key) {
-            if (!hana::contains(this->definitions, key)) {
-                fmt::print("run -- Undefined key {}\n", key.c_str());
-                std::exit(2);
-            }
-            auto& item = this->definitions[key];
-            auto& res = hana::at_c<4>(item);
-            if (res) {
-                // Already scheduled
-                // Return future for use in downstream calculations
-                return *res;
-            }
-            auto inputs = hana::at_c<1>(item);
-            // Schedule this function
-            auto func = hana::at_c<2>(item);
-            if constexpr (hana::is_empty(inputs)) {
-                res = new hpx::shared_future{hpx::async(func.get())};
+        auto run = hana::fix([this, &dataflow, &ec](auto self, auto key) {
+            if constexpr (hana::is_a<sch::input_tag>(key)) {
+                hpx::shared_future fut = hpx::make_ready_future(hana::at_key(ec, key.name));
+                return fut;
             }
             else {
-                // Schedule every input
-                auto input_res = hana::transform(inputs, self);
-                res = new hpx::shared_future{hana::unpack(
-                      input_res, hana::partial(dataflow, hpx::unwrapping(func.get())))};
+                auto& item = this->definitions[key];
+                auto& res = hana::at_c<4>(item)(ec);
+                if (res) {
+                    // Already scheduled
+                    // Return future for use in downstream calculations
+                    return *res;
+                }
+                auto inputs = hana::at_c<1>(item);
+                // Schedule this function
+                auto func = hana::at_c<2>(item);
+                if constexpr (hana::is_empty(inputs)) {
+                    fmt::print("Scheduling {} with no inputs\n", key.c_str());
+                    res = new hpx::shared_future{hpx::async(func)};
+                }
+                else {
+                    // Schedule every input
+                    auto input_res = hana::transform(inputs, self);
+                    fmt::print("Scheduling calculation of {} with inputs\n", key.c_str());
+                    res = new hpx::shared_future{hana::unpack(
+                          input_res, hana::partial(dataflow, hpx::unwrapping(func)))};
+                }
+                // Return future to be used as input in downstream calculations
+                return *res;
             }
-            fmt::print("Scheduled calculation of {}\n", key.c_str());
-            // Return future to be used as input in downstream calculations
-            return *res;
         });
         auto run_if_required = [this, &run, &is_required](auto key) {
             auto& item = this->definitions[key];
@@ -91,4 +111,9 @@ template <class... Defs> class Sched {
 };
 
 } // namespace sch
+
+template <typename CharT, CharT... str> constexpr auto operator""_in() {
+    return sch::Input{hana::string_c<str...>};
+}
+
 #endif /* HPXSCHED_H */
--- src/hpx_main.cpp
+++ src/hpx_main.cpp
@@ -7,48 +7,50 @@
 
 using namespace std::chrono_literals;
 
+int plus(int x, int y) {
+    fmt::print("Running plus({}, {})\n", x, y);
+    std::this_thread::sleep_for(1s);
+    return x + y;
+}
+
+int times(int x, int y) {
+    fmt::print("Running times({}, {})\n", x, y);
+    std::this_thread::sleep_for(1s);
+    return x * y;
+}
+
+int square(int x) {
+    fmt::print("Running square({})\n", x);
+    std::this_thread::sleep_for(1s);
+    return x * x;
+}
+
+int cube(int x) {
+    fmt::print("Running cube({})\n", x);
+    std::this_thread::sleep_for(100s);
+    return x * x * x;
+}
+
+sch::Sched scheduler{
+      sch::Define("Cube Plus"_s, hana::make_tuple("Ten plus Five"_s), cube),
+      sch::Define("Cube Times"_s, hana::make_tuple("Ten times Five"_s), cube),
+      sch::Define("Ten plus Five"_s, hana::make_tuple("Five"_in, "Ten"_in), plus),
+      sch::Define("Ten times Five"_s, hana::make_tuple("Five"_in, "Ten"_in), times),
+      sch::Define("Square Plus"_s, hana::make_tuple("Ten plus Five"_s), square),
+      sch::Define("Square Times"_s, hana::make_tuple("Ten times Five"_s), square),
+      sch::Define("Add Squares"_s, hana::make_tuple("Square Plus"_s, "Square Times"_s), plus)};
+struct EvtCtx : public decltype(scheduler)::ECBase {
+    int Five = 5;
+    int Ten = 10;
+};
+BOOST_HANA_ADAPT_STRUCT(EvtCtx, Five, Ten);
+
 int main(int argc, char* argv[]) {
-    constexpr auto plus = [](int x, int y) {
-        std::this_thread::sleep_for(1s);
-        return x + y;
-    };
-    constexpr auto times = [](int x, int y) {
-        std::this_thread::sleep_for(1s);
-        return x * y;
-    };
-    constexpr auto square = [](int x) {
-        std::this_thread::sleep_for(1s);
-        return x * x;
-    };
-    constexpr auto cube = [](int x) {
-        std::this_thread::sleep_for(100s);
-        return x * x * x;
-    };
-    sch::Sched scheduler{
-          sch::Define("Cube Plus"_s, hana::make_tuple("Ten plus Five"_s), cube),
-          sch::Define("Cube Times"_s, hana::make_tuple("Ten times Five"_s), cube),
-          sch::Define("Five"_s, hana::make_tuple(), []() { return 5; }),
-          sch::Define("Ten"_s, hana::make_tuple(), []() { return 10; }),
-          sch::Define("Ten plus Five"_s, hana::make_tuple("Five"_s, "Ten"_s), plus),
-          sch::Define("Ten times Five"_s, hana::make_tuple("Five"_s, "Ten"_s), times),
-          sch::Define("Square Plus"_s, hana::make_tuple("Ten plus Five"_s), square),
-          sch::Define("Square Times"_s, hana::make_tuple("Ten times Five"_s), square),
-          sch::Define("Add Squares"_s, hana::make_tuple("Square Plus"_s, "Square Times"_s), plus)};
-    auto& final_ans = scheduler.retrieve("Add Squares"_s);
-    auto& final_ans2 = scheduler.retrieve("Square Plus"_s);
-    auto& final_ans3 = scheduler.retrieve("Cube Times"_s);
-    bool success = scheduler.schedule();
-    if (!success) {
-        fmt::print("Failed to schedule!\n");
-        return 1;
-    }
-    fmt::print("Waiting on final_ans\n");
-    final_ans->wait();
-    fmt::print("Waiting on final_ans2\n");
-    final_ans2->wait();
-    fmt::print("Waiting on final_ans2\n");
-    final_ans3->wait();
-    fmt::print("Final answers are {}, {}, {}\n", final_ans->get(), final_ans2->get(),
-               final_ans3->get());
+    EvtCtx ec{};
+    ec.Five = 6;
+    ec.Ten = 11;
+    auto& final_ans = scheduler.retrieve(ec, "Add Squares"_s);
+    bool success = scheduler.schedule(ec);
+    fmt::print("Final answers are {}\n", final_ans->get());
     return 0;
 }
